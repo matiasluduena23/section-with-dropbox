dfea48e64eae484ec96deaca7f435352a8ac38f5
Matias Ludueña <matiasluduena23@gmail.com>
2023-09-18 19:45:47 -0300

    release 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,27 +4,39 @@ const span = document.querySelector('.line');
 window.addEventListener('resize', () => {
     if (window.innerWidth > 900) {
         span.appendChild(space);
+        closeNavOnResize()
     }
 });
 
+function closeNavOnResize(){
+    document.querySelectorAll('.active').forEach(e => e.classList.remove('active'))
+    const link = document.querySelector('.hamburger');
+        closeMenu(link)
+}
 
-const itemFeature = document.querySelector('.item-feature')
-const optionFeature = document.querySelector('.option-feature')
 
-itemFeature.addEventListener('click', ()=> {
-    optionFeature.classList.toggle('active')
+const ariaExpanded = (linkClass, optionClass) => {
+    const nav = document.querySelector(optionClass)
+    const link = document.querySelector(linkClass);
 
-;})
+    link.addEventListener('click', ()=> {
+        const isOpened = link.getAttribute('aria-expanded') === 'true';
+        if(isOpened ? closeMenu(link) : openMenu(link));
+        
+        nav.classList.toggle('active')
+    });
+}
 
-const itemCompany = document.querySelector('.item-company')
-const optionCompany = document.querySelector('.option-company')
+ariaExpanded('.hamburger','.primary-navigation' )
+ariaExpanded('.item-feature','.option-feature' )
+ariaExpanded('.item-company','.option-company' )
 
-itemCompany.addEventListener('click', ()=> {
-    optionCompany.classList.toggle('active')
-})
 
-const nav = document.querySelector('.nav')
-const hamburger = document.querySelector('.hamburger').addEventListener('click', ()=> {
-    nav.classList.toggle('expanded')
-})
 
+function openMenu(link){
+    link.setAttribute('aria-expanded', 'true' )
+}
+
+function closeMenu(link){
+    link.setAttribute('aria-expanded', 'false' )
+}
